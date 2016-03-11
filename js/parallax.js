cc55f03e9e07945a81ce507d10f2b40076a6f98d
manish2296lion <manish2296lion@gmail.com>
2016-03-11 19:23:02 +0530

    some more fixes
    fixed extra margin

--- js/parallax.js
+++ js/parallax.js
@@ -1,25 +1,6 @@
  //most the value you can get by experimentation
 $(function()
 {
-	$(window).scroll(function(){
-
-		var winscroll=$(this).scrollTop();
-		console.log('hi');
-		var winscroll2=winscroll-$(window).height()/1.45	;
-		
-		if($(this).scrollTop()<545)//Optimization Technique
-		{
-			
-		$('.logo').css({
-			'transform' : 'translate(0px,'+winscroll/1.5+'%)'
-		})
-
-		$('.bird').css({
-			'transform' : 'translate(0px,-'+winscroll/32+'%)'
-		})
-		}
-		}	
-		)
 	$(".fig").click(function(){
 		console.log('hi');	
 		var this_figure=$(this).attr('rel');
